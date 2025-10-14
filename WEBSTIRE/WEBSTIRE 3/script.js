f462c7b9133dd75fcf95d68d26aee4ff12b3dd81
Angel5l <angelgabrielzapanta@gmail.com>
2025-10-14 11:03:11 +0800

    Add files via upload

---
+++ WEBSTIRE/WEBSTIRE 3/script.js
@@ -0,0 +1,14 @@
+
+//Full Name: Zapanta, Angel Gabriel
+//Section: CS-202
+//Activity: Final Website
+//Date: 10/08/2025
+
+function myFunction() {
+  var x = document.getElementById("Testimonies");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
